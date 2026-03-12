a9e4f0fbf45d6b38fccf4275280320d2294ba90e
uniho <uniho@users.noreply.github.com>
2026-03-12 09:42:07 +0900

    v12.54.1

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc514d8f7b9fc5970bdd94287905ef584::getLoader();
+return ComposerAutoloaderInit53b5d56b3b7e3cbac1713e68c8850f6c::getLoader();
